c06ba7f23b56583fe14ea5febd5539d6877f943d
chairil ashar <asharchairil4@gmail.com>
2023-03-13 13:33:11 +0700

    routing

---
+++ src/pages/GetStarted/index.jsx
@@ -0,0 +1,44 @@
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {ILGetStarted, ILLogo} from '../../assets';
+import {Button, Gap} from '../../components';
+
+export default function GetStarted({navigation}) {
+  return (
+    <ImageBackground source={ILGetStarted} style={styles.page}>
+      <View>
+        <ILLogo />
+        <Text style={styles.title}>
+          Konsultasi dengan dokter jadi lebih mudah & fleksibel
+        </Text>
+      </View>
+      <View>
+        <Button
+          title="Get Started"
+          onPress={() => navigation.navigate('Register')}
+        />
+        <Gap height={16} />
+        <Button
+          type="secondary"
+          title="Sign In"
+          onPress={() => navigation.replace('Login')}
+        />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 40,
+    justifyContent: 'space-between',
+    backgroundColor: 'black',
+    flex: 1,
+  },
+  title: {
+    fontSize: 28,
+    marginTop: 91,
+    color: 'white',
+    fontFamily: 'Nunito-SemiBold',
+  },
+});
