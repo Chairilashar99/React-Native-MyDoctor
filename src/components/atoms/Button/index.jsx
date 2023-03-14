50dc097ee336513581fb3748a2f38b664b74653f
chairil ashar <asharchairil4@gmail.com>
2023-03-14 16:28:46 +0700

    refactoring component

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -1,8 +1,12 @@
 import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {colors} from '../../../utils';
+import IconOnly from './IconOnly';
 
-export default function Button({type, title, onPress}) {
+export default function Button({type, title, onPress, icon}) {
+  if (type === 'icon-only') {
+    return <IconOnly icon={icon} onPress={onPress} />;
+  }
   return (
     <TouchableOpacity style={styles.container(type)} onPress={onPress}>
       <Text style={styles.text(type)}>{title}</Text>
@@ -21,7 +25,6 @@ const styles = StyleSheet.create({
   }),
   text: type => ({
     fontSize: 18,
-    fontWeight: '600',
     fontFamily: 'Nunito-SemiBold',
     textAlign: 'center',
     color:
