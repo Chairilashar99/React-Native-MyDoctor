7bb9c03c799bdfd2d92a87ff6cce0a6bede7db96
chairil ashar <asharchairil4@gmail.com>
2023-03-15 22:11:52 +0700

    bottom tab navigator

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Splash, GetStarted, Register, Login, UploadPhoto} from '../pages';
+import {
+  Splash,
+  GetStarted,
+  Register,
+  Login,
+  UploadPhoto,
+  Doctor,
+  Messages,
+  Hospitals,
+} from '../pages';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Doctor"
+        component={Doctor}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={Messages}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Hospitals"
+        component={Hospitals}
+        options={{headerShown: false}}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Router = () => {
   return (
@@ -30,6 +63,11 @@ const Router = () => {
       <Stack.Screen
         name="UploadPhoto"
         component={UploadPhoto}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
