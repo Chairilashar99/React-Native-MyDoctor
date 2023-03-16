1ce94214ba34aca70eb1b1157bab62241fd6a1ba
chairil ashar <asharchairil4@gmail.com>
2023-03-16 22:50:08 +0700

    base component

---
+++ src/components/molecules/HomeProfile/index.jsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function HomeProfile() {
+  return (
+    <View>
+      <Text>Home Profile Component</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
