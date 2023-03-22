c863e36162bc0c83bb8623600e22c970494232b3
chairil ashar <asharchairil4@gmail.com>
2023-03-22 22:19:53 +0700

    screen chat

---
+++ src/components/molecules/InputChat/index.jsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function InputChat() {
+  return (
+    <View>
+      <Text>Input Chat Component</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
