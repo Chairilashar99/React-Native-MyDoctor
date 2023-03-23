ce3cf0c4ebde7faa6a65146f614ac90ac83c1cc2
chairil ashar <asharchairil4@gmail.com>
2023-03-24 05:11:04 +0700

    text input

--- src/pages/Chatting/index.jsx
+++ src/pages/Chatting/index.jsx
@@ -5,18 +5,27 @@ import {colors, fonts} from '../../utils';
 
 export default function Chatting() {
   return (
-    <View>
+    <View style={styles.page}>
       <Header type="dark-profile" title="Nairobi Putri Hayza" />
-      <Text style={styles.chatDate}>Senin, 21 Maret, 2023</Text>
-      <ChatItem />
-      <ChatItem />
-      <ChatItem />
+      <View style={styles.content}>
+        <Text style={styles.chatDate}>Senin, 21 Maret, 2023</Text>
+        <ChatItem />
+        <ChatItem />
+        <ChatItem />
+      </View>
       <InputChat />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  page: {
+    backgroundColor: colors.white,
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+  },
   chatDate: {
     fontSize: 11,
     fontFamily: fonts.primary.normal,
