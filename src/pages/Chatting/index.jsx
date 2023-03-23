9d84115a43fdf88ac166706ff2d945fed530eb8a
chairil ashar <asharchairil4@gmail.com>
2023-03-23 18:17:00 +0700

    slicing chating

--- src/pages/Chatting/index.jsx
+++ src/pages/Chatting/index.jsx
@@ -1,12 +1,13 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {ChatItem, Header, InputChat} from '../../components';
+import {colors, fonts} from '../../utils';
 
 export default function Chatting() {
   return (
     <View>
-      <Header title="Nairobi Putri Hayza" />
-      <Text>Senin, 21 Maret, 2023</Text>
+      <Header type="dark-profile" title="Nairobi Putri Hayza" />
+      <Text style={styles.chatDate}>Senin, 21 Maret, 2023</Text>
       <ChatItem />
       <ChatItem />
       <ChatItem />
@@ -15,4 +16,12 @@ export default function Chatting() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  chatDate: {
+    fontSize: 11,
+    fontFamily: fonts.primary.normal,
+    color: colors.text.secondary,
+    marginVertical: 20,
+    textAlign: 'center',
+  },
+});
